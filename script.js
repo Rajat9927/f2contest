2ec0e2c34631e8db739842cf42cd1a2f9504aedb
Rajat9927 <Rajat9927kumar@gmail.com>
2023-01-07 12:47:02 +0530

    push

--- script.js
+++ script.js
@@ -6,22 +6,31 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+
 function PrintDeveloperswithMap() {
   //Write your code here
+  //map function syntatx array.map(function(currentValue, index, arr), thisValue)
+  arr.map(developerfilter);
+  function developerfilter(arrayData){
+    if(arrayData.profession==="developer"){
+      console.log(arrayData);
+    }
+  }
+  
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
-}
+      //Write your code here
+    }
 
 function addData() {
-  //Write your code here
-}
+      //Write your code here
+    }
 
 function removeAdmin() {
-  //Write your code here
-}
+      //Write your code here
+    }
 
 function concatenateArray() {
-  //Write your code here
-}
+      //Write your code here
+    }
